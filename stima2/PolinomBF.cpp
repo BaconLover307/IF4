91544c12e50274ab00bf199ff04da4206a1c5537
BaconLover307 <kresnadi30@gmail.com>
2020-02-18 16:19:12 +0700

    Mulai stima2

--- stima2/PolinomBF.cpp
+++ stima2/PolinomBF.cpp
@@ -10,32 +10,32 @@
 
 using namespace std;
 
-PolnomBF::PolnomBF() {
+PolinomBF::PolinomBF() {
     this -> derajat = 0;
     for (int i = 0; i < MAX_LENGTH; i++) {
         this -> koef[i] = 0;
     }
 }
 
-PolnomBF::PolnomBF(int derajat) {
+PolinomBF::PolinomBF(int derajat) {
     this -> derajat = derajat;
     for (int i = 0; i < MAX_LENGTH; i++) {
         this -> koef[i] = 0;
     }
 }
 
-PolnomBF::PolnomBF(const PolnomBF& other) {
+PolinomBF::PolinomBF(const PolinomBF& other) {
     this -> derajat = other.derajat;
     for (int i = 0; i < MAX_LENGTH; i++) {
         this -> koef[i] = other.koef[i];
     }
 }
 
-PolnomBF::~PolnomBF() {
+PolinomBF::~PolinomBF() {
     // tidak ada new, maka kosong
 }
 
-PolnomBF& PolnomBF::operator=(const PolnomBF& P) {
+PolinomBF& PolinomBF::operator=(const PolinomBF& P) {
     derajat = P.derajat;
     for (int i=0; i<MAX_LENGTH; i++) {
         koef[i] = P.koef[i];
@@ -43,21 +43,21 @@ PolnomBF& PolnomBF::operator=(const PolnomBF& P) {
     return *this;
 }
 
-int PolnomBF::getKoefAt(int idx) const {
+int PolinomBF::getKoefAt(int idx) const {
     return koef[idx];
 }
-int PolnomBF::getDerajat() const {
+int PolinomBF::getDerajat() const {
     return derajat;
 }
-void PolnomBF::setKoefAt(int idx, int val) {
+void PolinomBF::setKoefAt(int idx, int val) {
     koef[idx] = val;
 }
-void PolnomBF::setDerajat(int idx) {
+void PolinomBF::setDerajat(int idx) {
     derajat = idx;
 }
 
-PolnomBF operator+(const PolnomBF & P1, const PolnomBF & P2) {
-    PolnomBF Ph;
+PolinomBF operator+(const PolinomBF & P1, const PolinomBF & P2) {
+    PolinomBF Ph;
     if (P1.getDerajat() > P2.getDerajat()) {
         Ph.setDerajat(P1.getDerajat());
     } else {
@@ -68,8 +68,8 @@ PolnomBF operator+(const PolnomBF & P1, const PolnomBF & P2) {
     }
     return Ph;
 }
-PolnomBF operator-(const PolnomBF & P1, const PolnomBF & P2) {
-    PolnomBF Ph;
+PolinomBF operator-(const PolinomBF & P1, const PolinomBF & P2) {
+    PolinomBF Ph;
     if (P1.getDerajat() > P2.getDerajat()) {
         Ph.setDerajat(P1.getDerajat());
     } else {
@@ -80,24 +80,24 @@ PolnomBF operator-(const PolnomBF & P1, const PolnomBF & P2) {
     }
     return Ph;
 }
-PolnomBF operator*(const PolnomBF & P, const int n) {
-    PolnomBF Ph;
+PolinomBF operator*(const PolinomBF & P, const int n) {
+    PolinomBF Ph;
     Ph.setDerajat(P.getDerajat());
     for (int i = 0; i < Ph.getDerajat(); i++) {
         Ph.koef[i] = P.koef[i] * n;
     }
     return Ph;
 }
-PolnomBF operator*(const int n, const PolnomBF & P) {
-    PolnomBF Ph;
+PolinomBF operator*(const int n, const PolinomBF & P) {
+    PolinomBF Ph;
     Ph.setDerajat(P.getDerajat());
     for (int i = 0; i < Ph.getDerajat(); i++) {
         Ph.koef[i] = n * P.koef[i];
     }
     return Ph;
 }
-PolnomBF operator/(const PolnomBF & P, const int n) {
-    PolnomBF Ph;
+PolinomBF operator/(const PolinomBF & P, const int n) {
+    PolinomBF Ph;
     Ph.setDerajat(P.getDerajat());
     for (int i = 0; i < Ph.getDerajat(); i++) {
         Ph.koef[i] = P.koef[i] / n;
@@ -105,19 +105,19 @@ PolnomBF operator/(const PolnomBF & P, const int n) {
     return Ph;
 }
 
-void PolnomBF::input() {
+void PolinomBF::input() {
     for (int i = 0; i<derajat; i++) {
         cin >> koef[i];
     }
 }
 
-void PolnomBF::printKoef() {
+void PolinomBF::printKoef() {
     for (int i = 0; i<derajat; i++) {
         cout << koef[i] << endl;
     }
 }
 
-int PolnomBF::substitute(int x) {
+int PolinomBF::substitute(int x) {
     int ret = 0, p = 1;
     for (int i = 0; i<derajat; i++) {
         ret += p * koef[i];
@@ -126,15 +126,15 @@ int PolnomBF::substitute(int x) {
     return ret;
 }
 
-// Melakukan aksi derivasi terhadap PolnomBF.
-// Lakukan pengurangan pada derajat tertinggi PolnomBF.
+// Melakukan aksi derivasi terhadap PolinomBF.
+// Lakukan pengurangan pada derajat tertinggi PolinomBF.
 // Apabila derajat tertinggi = 0, hasil derivasi = 0 (dengan derajat tertinggi = 0)
-PolnomBF PolnomBF::derive() {
+PolinomBF PolinomBF::derive() {
     if (derajat == 0) {
-        PolnomBF Ph;
+        PolinomBF Ph;
         return Ph;
     } else {
-        PolnomBF Ph(derajat-1);
+        PolinomBF Ph(derajat-1);
         for (int i = 0; i<Ph.derajat; i++) {
             Ph.koef[i] = koef[i+1] * (i+1);
         }
@@ -147,7 +147,7 @@ PolnomBF PolnomBF::derive() {
 // Apabila suatu koefisien bernilai < 0, gunakan tanda "-" untuk menggantikan tanda "+"
 // Apabila suatu koefisien bernilai 0, lewati koefisien tersebut dan lanjutkan ke koefisien selanjutnya
 // Jika seluruh koefisien bernilai 0, keluarkan "0"
-void PolnomBF::print() {
+void PolinomBF::print() {
     cout << koef[0];
     for (int i = 1; i <= derajat; i++) {
         if (koef[i] < 0) cout << koef[i] << "x^" << i;
